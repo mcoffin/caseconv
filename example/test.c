dfabb2457afdf067df7f39c66c592c8118965beb
Matt Coffin <matt.coffin@charter.com>
2017-03-30 13:31:56 -0600

    example: convert all arguments

--- example/test.c
+++ example/test.c
@@ -3,8 +3,10 @@
 #include <caseconv.h>
 
 int main(int argc, char **argv) {
-	char *converted = caseconv_guess_and_convert(argv[1], CASECONV_CASE_TYPE_KEBAB);
-	printf("%s\n", converted);
-	free(converted);
+	for (int i = 1; i < argc; i++) {
+		char *converted = caseconv_guess_and_convert(argv[i], CASECONV_CASE_TYPE_KEBAB);
+		printf("%s\n", converted);
+		free(converted);
+	}
 	return 0;
 }
